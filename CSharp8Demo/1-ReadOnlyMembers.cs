09fb34d26d9e8805aa65ec3185d6280806f7c7ee
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-02-08 07:49:19 +0000

    Add project files.

---
+++ CSharp8Demo/1-ReadOnlyMembers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8Demo
+{
+   public struct Rectangle
+    {
+        public double Length { get; set; }
+        public double Height { get; set; }
+        
+        
+        // FULL PROPERTY will result in implicit copy in the readonly method
+        //private int _height;
+
+        //public int Height
+        //{
+        //    get { return _height; }
+        //    set { _height = value; }
+        //}
+
+        public readonly double Area()
+        {
+            return Length * Height;
+            //Height += 1;  // DOES NOT WORK AS THE METHOD IS READONLY
+        }
+    }
+}
---
+++ CSharp8Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp8Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
