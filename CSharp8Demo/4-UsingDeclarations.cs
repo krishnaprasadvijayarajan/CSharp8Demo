dfc9fe7760e9772028e970cafeb6e26100aeb370
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-02-11 22:31:53 +0000

    Using Declarations

--- CSharp8Demo/4-UsingDeclarations.cs
+++ CSharp8Demo/4-UsingDeclarations.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CSharp8Demo
 {
     public static class UsingDeclarations
     {
+        public static int ConvertFiles()
+        {
+            int output = 0;
+            using var inputFile = new StreamReader("TestFile.txt");
+            using var outputFile = new StreamWriter("OutputFile.txt");
+            string line;
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                outputFile.WriteLine(line);
+                output++;
+            }
+            //using (var inputFile = new StreamReader("TestFile.txt"))
+            //{
+            //    using (var outputFile = new StreamWriter("OutputFile.txt"))
+            //    {
+            //        string line;
+            //        while((line=inputFile.ReadLine())!=null)
+            //        {
+            //            outputFile.WriteLine(line);
+            //            output++;
+            //        }
+            //    }
+            //}
+            return output;
+        }
     }
 }
